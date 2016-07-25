0ead6dd0a91cb68cfe245b5dcf214352db9a1cf6
3rcuncle <lxxr.goodluck@gmail.com>
2016-07-25 10:23:25 +0800

    reservoid to Class

--- random/reservoid_sampling.py
+++ random/reservoid_sampling.py
@@ -2,17 +2,26 @@
 import sys
 import numpy as np
 
-size = int(sys.argv[1])
-pool = []
-for index, line in enumerate(sys.stdin):
-	line = line.rstrip('\n')
-	if index < size:
-		pool.append(line)
-	else:
+class Reservoid(object):
+	def __init__(self, size):
+		self.sie = size
+		self.pool = []
+	
+	def add(self, item):
+		if len(self.pool) < size:
+			self.pool.append(line)
+			return
 		# randint(index) 从[0,index)中随机取值
 		m = np.random.randint(index + 1)
 		if (m < size):
-			pool[m] = line
+			self.pool[m] = item
+		return
 
-for line in pool:
-	print line
+if __name__ == '__main__':
+	size = int(sys.argv[1])
+	reservoid = Reservoid(size)
+	for index, line in enumerate(sys.stdin):
+		line = line.rstrip('\n')
+		reservoid.add(line)
+	for line in reservoid.pool:
+		print line
